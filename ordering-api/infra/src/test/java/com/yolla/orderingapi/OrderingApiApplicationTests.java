2bf948e1f64963c71b8277b642108d31b991102d
semihsahan <semih.sahan@migrosonline.com>
2026-09-20 20:00:26 +0300

    feat: configure modulith event publication registry

--- ordering-api/infra/src/test/java/com/yolla/orderingapi/OrderingApiApplicationTests.java
+++ ordering-api/infra/src/test/java/com/yolla/orderingapi/OrderingApiApplicationTests.java
@@ -2,12 +2,14 @@ package com.yolla.orderingapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OrderingApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoadsWithOrderAndModulithInfrastructure() {
     }
 
 }
